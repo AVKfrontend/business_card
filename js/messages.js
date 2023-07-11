5a841ef97d0a610e4c09fe679b1aa49cf29e4ca9
AVKfrontend <aleks_vk@ukr.net>
2023-07-11 22:33:23 +0300

    Added description in modal window and Git courses certificate
    Signed-off-by: AVKfrontend <aleks_vk@ukr.net>

---
+++ js/messages.js
@@ -0,0 +1,24 @@
+export const messages = {
+	order: {
+		uk: `<p class="cont__text">Замовити верстку можна електронною поштою: <a href="mailto:aleks_vk@ukr.net" class="cont__link">aleks_vk@ukr.net</a>; по телефону: <a href="tel:380964662905" class="cont__link">+38(096)466-29-05</a>; написати в <a class="cont__link" href="https://t.me/a_v_kuk" rel="nofollow noopener noreferrer" target="_blank">Telegram</a>; або на моїх облікових записах фрілансбірж: </p>
+			<!-- noindex -->
+			<a href="https://www.upwork.com/freelancers/~012b3af5cb896500b3" class="cont__link" rel="noreferrer noreferrer nofollow" target="_blank">UpWork</a>; 
+			<a href="https://freelancehunt.com/freelancer/AVKprodakshn.html" class="cont__link" rel="noreferrer noreferrer nofollow" target="_blank">Freelancehunt</a>
+			<!-- /noindex -->`,
+		en: `<p class="cont__text">You can order layout by e-mail: <a href="mailto:aleks_vk@ukr.net" class="cont__link">aleks_vk@ukr.net</a>; by phone: <a href="tel:380964662905" class="cont__link">+38(096)466-29-05</a>; write to <a class="cont__link" href="https://t.me/a_v_kuk" rel="nofollow noopener noreferrer" target="_blank">Telegram</a>; or on my freelance services accounts: </p>
+			<!-- noindex -->
+			<a href="https://www.upwork.com/freelancers/~012b3af5cb896500b3" class="cont__link" rel="noreferrer noreferrer nofollow" target="_blank">UpWork</a>; 
+			<a href="https://freelancehunt.com/freelancer/AVKprodakshn.html" class="cont__link" rel="noreferrer noreferrer nofollow" target="_blank">Freelancehunt</a>
+			<!-- /noindex -->`,
+		ru: `<p class="cont__text">Заказать верстку можно по електронной почте: <a href="mailto:aleks_vk@ukr.net" class="cont__link">aleks_vk@ukr.net</a>; по телефону: <a href="tel:380964662905" class="cont__link">+38(096)466-29-05</a>; написать в <a class="cont__link" href="https://t.me/a_v_kuk" rel="nofollow noopener noreferrer" target="_blank">Telegram</a>; или на моих аккаунтах фрилансбирж: </p>
+			<!-- noindex -->
+			<a href="https://www.upwork.com/freelancers/~012b3af5cb896500b3" class="cont__link" rel="noreferrer noreferrer nofollow" target="_blank">UpWork</a>; 
+			<a href="https://freelancehunt.com/freelancer/AVKprodakshn.html" class="cont__link" rel="noreferrer noreferrer nofollow" target="_blank">Freelancehunt</a>
+			<!-- /noindex -->`
+	},
+	m_at_h: {
+		uk: `<p class="cont__text">Сайт виконаний на фреймворку Vue.js (3) як однофайловий додаток.</p> <p class="cont__text">Дані, що відображаються, надходять з трьох різних вільних API. (https://metmuseum.github.io/ - The Metropolitan Museum of Art, https://api.artic.edu/docs/ - Art Institute of Chicago API, https://pro.europeana.eu/page/apis - Europeana Foundation).</p> <p class="cont__text">Результат можна подивитися на <a class="cont__link" ref="https://museumathome.onrender.com/" rel="nofollow noopener noreferrer" target="_blank">https://museumathome.onrender.com/</a></p>`,
+		en: `<p class="cont__text">The site is made on the Vue.js (3) framework as a single-file application.</p> <p class="cont__text">Displayed data comes from three different free APIs. (https://metmuseum.github.io/ - The Metropolitan Museum of Art, https://api.artic.edu/docs/ - Art Institute of Chicago API, https://pro.europeana.eu/page/apis - Europeana Foundation).</p> <p class="cont__text">The result can be viewed at <a class="cont__link" ref="https://museumathome.onrender.com/" rel="nofollow noopener noreferrer" target="_blank">https://museumathome.onrender.com/</a></p>`,
+		ru: `<p class="cont__text">Сайт выполнен на фреймворке Vue.js (3) как однофайловое приложение.</p> <p class="cont__text">Отображаемые данные поступают с трех разных свободных API. (https://metmuseum.github.io/ - The Metropolitan Museum of Art, https://api.artic.edu/docs/ - Art Institute of Chicago API, https://pro.europeana.eu/page/apis - Europeana Foundation).</p> <p class="cont__text">Результат можно посмотреть на <a class="cont__link" ref="https://museumathome.onrender.com/" rel="nofollow noopener noreferrer" target="_blank">https://museumathome.onrender.com/</a></p>`
+	},
+};
